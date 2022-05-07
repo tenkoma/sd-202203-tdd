6b73dcf31ea61d76d2774d696f8260d467c75da0
Koji Tanaka <tenkoma@gmail.com>
2022-05-07 15:13:50 +0900

    feat: 2を渡すと文字列'2'を返す

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function fizzBuzz(num) {
-  return '1';
+  return num.toString();
 }
 
 module.exports = fizzBuzz;
--- index.test.js
+++ index.test.js
@@ -3,3 +3,7 @@ const fizzBuzz = require('./index');
 test("1を渡すと文字列'1'を返す", () => {
   expect(fizzBuzz(1)).toBe('1');
 });
+
+test("2を渡すと文字列'2'を返す", () => {
+  expect(fizzBuzz(2)).toBe('2');
+});
